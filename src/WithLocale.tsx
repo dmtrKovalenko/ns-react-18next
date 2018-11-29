7f8ef4c465bdda00543e785697a04c9611cbce0c
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-11-29 17:46:36 +0200

    Make WithLocale use ns from context

--- src/WithLocale.tsx
+++ src/WithLocale.tsx
@@ -1,6 +1,7 @@
-import * as React from 'react'
-import * as i18n from 'i18next';
-import { I18NextConsumer } from './I18NextProvider';
+import * as React from "react";
+import * as i18n from "i18next";
+import { I18NextConsumer } from "./I18NextProvider";
+import { NameSpaceConsumer } from "./NamespaceProvider";
 
 export interface WithLocaleProps {
   t: typeof i18n.t;
@@ -9,24 +10,28 @@ export interface WithLocaleProps {
   tn: (key: string, inerpolate?: { [key: string]: string }) => string;
 }
 
-export default (defaultNs?: string) => (Component: React.ComponentType<WithLocaleProps>) => {
-  const WithLocale: React.SFC<any> = (props) => (
+export default (defaultNs?: string) => (
+  Component: React.ComponentType<WithLocaleProps>
+) => {
+  const WithLocale: React.SFC<any> = props => (
     <I18NextConsumer>
-      {
-        ({ i18n, locale }) => (
-          <Component
-            t={i18n.t}
-            i18n={i18n}
-            locale={locale}
-            tn={(key: string, inerpolate?: { [key: string]: string }) => (
-              i18n.t(`${defaultNs}:${key}`, inerpolate)
-            )}
-            {...props}
-          />
-        )
-      }
+      {({ i18n, locale }) => (
+        <NameSpaceConsumer>
+          {ns => (
+            <Component
+              t={i18n.t}
+              i18n={i18n}
+              locale={locale}
+              tn={(key: string, interpolate?: { [key: string]: string }) =>
+                i18n.t(`${defaultNs || ns}:${key}`, interpolate)
+              }
+              {...props}
+            />
+          )}
+        </NameSpaceConsumer>
+      )}
     </I18NextConsumer>
-  )
+  );
 
-  return WithLocale
-}
+  return WithLocale;
+};
